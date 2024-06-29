91d51c649a81e16894bc3f01ba53f29be80f654e
Bhavin17041998 <bhavin17.panchal@gmail.com>
2024-06-29 13:21:50 +0530

    added remaining content

--- send_mail.php
+++ send_mail.php
@@ -4,31 +4,53 @@ use PHPMailer\PHPMailer\Exception;
 
 require 'vendor/autoload.php';
 
-$mail = new PHPMailer(true);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $title = "Service Enquiry";
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-try {
-    // Server settings
-    $mail->SMTPDebug = 0;
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com'; // Gmail SMTP server
-    $mail->SMTPAuth = true;
-    $mail->Username = 'your_gmail_address@gmail.com'; // Your Gmail address
-    $mail->Password = 'your_gmail_password'; // Your Gmail password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = 587;
+    $mail = new PHPMailer(true);
 
-    // Recipients
-    $mail->setFrom('your_gmail_address@gmail.com', 'Your Name');
-    $mail->addAddress('bhavinkumar318@gmail.com', 'Bhavin'); // Add a recipient
+    try {
+        // Enable verbose debug output
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com'; // Gmail SMTP server
+        $mail->SMTPAuth = true;
+        $mail->Username = 'bhavin17.panchal@gmail.com'; // Your Gmail address
+        $mail->Password = 'mtdn ngis ejfe kcjo'; // Your Gmail app password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
 
-    // Content
-    $mail->isHTML(true);
-    $mail->Subject = 'Test email';
-    $mail->Body    = 'This is a test email.';
+        // Recipients
+        $mail->setFrom('bhavin17.panchal@gmail.com', 'Bhavin Panchal');
+        $mail->addAddress('bhavinkumar318@gmail.com', 'Bhavin'); // Add a recipient
 
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        
+        // Body with h1 tag and parameters
+        $mail->Body = "
+        <h1>$title</h1>
+        <p><strong>Name:</strong> $name</p>
+        <p><strong>Email:</strong> $email</p>
+        <p><strong>Subject:</strong> $subject</p>
+        <p><strong>Message:</strong> $message</p>
+        ";
+
+        $mail->send();
+        if($_POST['redirect'] == 'contact'){
+            header('Location: contact.html?success=true');
+        }else if($_POST['redirect'] == 'quote'){
+            header('Location: quote.html?success=true');
+        }else{
+            header('Location: index.html?success=true');
+        }
+        exit();
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 }
 ?>
